d45c02faeeaf5a8313bdf93aee2a7f34ea961961
Ben Lee <bendelonlee@gmail.com>
2018-10-31 09:31:51 -0600

    First part of Curator test passes

--- lib/curator.rb
+++ lib/curator.rb
@@ -2,6 +2,15 @@ require './lib/artist'
 require './lib/photograph'
 
 class Curator
-  def initialize(arg)
+  attr_reader :photographs, :artists
+  def initialize(arg = nil)
+    @photographs = []
+    @artists = []
+  end
+  def add_artist(attributes)
+    @artists << Artist.new(attributes)
+  end
+  def add_photograph(attributes)
+    @photographs << Photograph.new(attributes)
   end
 end
