b7a66e76e0ba6fa8756a7f2bad5137ecd59ea77d
Ben Lee <bendelonlee@gmail.com>
2018-10-31 09:20:33 -0600

    Test passes

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,9 +1,10 @@
-class Atrist
-  attr_reader :id, :name, :artist_id, :year
+class Artist
+  attr_reader :id, :name, :born, :died, :country
   def initialize(attributes)
     @id = attributes[:id]
     @name = attributes[:name]
-    @artist_id = attributes[:artist_id]
-    @year = attributes[:year]
+    @born = attributes[:born]
+    @died = attributes[:died]
+    @country = attributes[:country]
   end
 end
--- test/artist_test.rb
+++ test/artist_test.rb
@@ -16,9 +16,10 @@ class ArtistTest < Minitest::Test
       country: "United States"
     }
     artist = Artist.new(attributes)
-    assert_equal 'Ansel Adams', artist.id
-    assert_equal '1902', artist.name
-    assert_equal '1984', artist.born
-    assert_equal 'United States', artist.died
+    assert_equal '2', artist.id
+    assert_equal 'Ansel Adams', artist.name
+    assert_equal '1902', artist.born
+    assert_equal '1984', artist.died
+    assert_equal 'United States', artist.country
   end
 end
