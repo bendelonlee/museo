fb8c2a047d8866499f83619ad02292604a343e3a
Ben Lee <bendelonlee@gmail.com>
2018-10-31 09:43:19 -0600

    Find by id tests pass

--- lib/curator.rb
+++ lib/curator.rb
@@ -13,4 +13,11 @@ class Curator
   def add_photograph(attributes)
     @photographs << Photograph.new(attributes)
   end
+  def find_photograph_by_id(id)
+    @photographs.find{ |photo| photo.id == id }
+  end
+
+  def find_artist_by_id(id)
+    @artists.find{ |artist| artist.id == id }
+  end
 end
